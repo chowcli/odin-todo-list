08438c047d6da56855686dcb91d99f2fd2ec06fc
chowcli <136085102+chowcli@users.noreply.github.com>
2023-10-02 19:45:45 +0700

    Add modal, change styling

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import "./styles/main.css";
 import "./styles/header.css";
 import "./styles/site-bar.css";
 import "./styles/content.css";
+import "./styles/modal.css";
 
 document.addEventListener("DOMContentLoaded", () => websiteInteract());
