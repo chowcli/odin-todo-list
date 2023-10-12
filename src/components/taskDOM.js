c905c88712dc8b0ea4baabe920fa4667424e615b
chowcli <136085102+chowcli@users.noreply.github.com>
2023-10-12 21:52:33 +0700

    Refactor the dom, separate into smaller function

--- src/components/taskDOM.js
+++ src/components/taskDOM.js
@@ -1,29 +1,81 @@
-function createTaskDOM(task) {
+function setPriorityClass(priority) {
+  const priorityClasses = {
+    low: "low-priority",
+    medium: "medium-priority",
+    high: "high-priority",
+  };
+
+  return priorityClasses[priority];
+}
+
+function leftPartDOM(task) {
   const leftPart = document.createElement("div");
   leftPart.classList.add("leftPart");
+
+  // button to store check icon
+  const checkBtn = document.createElement("button");
+  checkBtn.classList.add("btn", "checkBtn");
   const checkIcon = document.createElement("i");
-  checkIcon.classList.add("bx-fw", "bx", "bx-checkbox");
+  checkIcon.classList.add("bx", "bx-checkbox");
+  checkBtn.appendChild(checkIcon);
+
+  // span element for task name
   const taskName = document.createElement("span");
   taskName.classList.add("task-name");
   taskName.textContent = task.name;
-  leftPart.append(checkIcon, taskName);
 
+  leftPart.append(checkBtn, taskName);
+
+  return leftPart;
+}
+
+function rightPartDOM(task) {
   const rightPart = document.createElement("div");
   rightPart.classList.add("rightPart");
+
   const dueDate = document.createElement("span");
   dueDate.classList.add("task-date");
   dueDate.textContent = task.dueDate;
+
+  // all buttons to store icons
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("btn", "editBtn");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("btn", "deleteBtn");
+  const infoBtn = document.createElement("button");
+  infoBtn.classList.add("btn", "infoBtn");
+
   const editIcon = document.createElement("i");
   editIcon.classList.add("bx", "bx-edit");
   const deleteIcon = document.createElement("i");
   deleteIcon.classList.add("bx", "bxs-trash");
   const infoIcon = document.createElement("i");
   infoIcon.classList.add("bx", "bx-info-circle");
-  rightPart.append(dueDate, editIcon, deleteIcon, infoIcon);
 
+  editBtn.appendChild(editIcon);
+  deleteBtn.appendChild(deleteIcon);
+  infoBtn.appendChild(infoIcon);
+  rightPart.append(dueDate, editBtn, deleteBtn, infoBtn);
+
+  return rightPart;
+}
+
+function createTaskDOM(task) {
+  const leftPart = leftPartDOM(task);
+  const rightPart = rightPartDOM(task);
+
+  // taskItem container
   const taskItem = document.createElement("div");
   taskItem.classList.add("task-item");
   taskItem.setAttribute("data-task-id", task.id);
+
+  // change color of text, icon and border hover based on priority
+  // state of item: checked or uncheck
+  console.log("Task priority:", task.priority);
+  const priorityClassName = setPriorityClass(task.priority);
+  console.log("Task priority class name:", priorityClassName);
+  taskItem.classList.add(`${priorityClassName}`, `check-${task.completed}`);
+
   taskItem.append(leftPart, rightPart);
 
   return taskItem;
